01a342aeb25d05f09a837d8d6585ab01b183405e
Chary.manu <chary.manu78@gmail.com>
2024-09-23 18:25:58 +0530

    I added some text

---
+++ 02.conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh : Hi ramesh, How are you."
+echo "Ramesh : Hi Suresh, I am fine, How are you doing."
+echo "Suresh : I am doing good Suresh. What's going on."
+echo "Ramesh : I started learning Shell Script Ramesh."
