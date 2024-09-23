321e5aed6c5deb8bd6085d8ee95628b922954e80
Chary.manu <chary.manu78@gmail.com>
2024-09-23 19:11:09 +0530

    Varibales text code added

--- 03.variables.sh
+++ 03.variables.sh
@@ -3,7 +3,7 @@
 PERSON1 = "Suresh"
 PERSON2 = "Ramesh"
 
-echo "$PERSON1 : Hi {$PERSON2}, How are you."
-echo "{$PERSON2} : Hi $PERSON1h, I am fine, How are you doing."
+echo "$PERSON1 : : Hi {$PERSON2}, How are you."
+echo "{$PERSON2} : Hi $PERSON1, I am fine, How are you doing."
 echo "$PERSON1 : I am doing good $PERSON1. What's going on."
 echo "{$PERSON2} : I started learning Shell Script {$PERSON2}."
