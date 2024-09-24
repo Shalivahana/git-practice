4801d07e066662e34a5a801fc826abeace7502e5
Chary.manu <chary.manu78@gmail.com>
2024-09-24 18:36:43 +0530

    array.sh

--- 06.array.sh
+++ 06.array.sh
@@ -5,3 +5,5 @@ FRUITS=("Orange" "Apple" "Kiwi")
 echo "first fruit name is: ${FRUITS[0]} "
 echo "second fruit name is: ${FRUITS[1]} "
 echo "second fruit name is: ${FRUITS[2]} "
+
+echo "first fruit name is: ${FRUITS[@]} "
