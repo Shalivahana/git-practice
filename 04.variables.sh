c552a9d4dc9de0b46ae7220bbbf0dd536a21545f
Chary.manu <chary.manu78@gmail.com>
2024-09-24 17:56:32 +0530

    array.sh

--- 04.variables.sh
+++ 04.variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PERSON1=$1
-PERSON2=$2
+PERSON2=$2 n7
 
 echo "$PERSON1 : : Hi $PERSON2, How are you."
 echo "$PERSON2 : : Hi $PERSON1, I am fine, How are you doing."
-echo "$PERSON1 : : I am doing good $PERSON1. What's going on."
-echo "$PERSON2 : : I started learning Shell Script $PERSON2."
+echo "$PERSON1 : : I am doing good $PERSON2. What's going on."
+echo "$PERSON2 : : I started learning Shell Script $PERSON1."
